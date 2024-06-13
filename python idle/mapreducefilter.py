7546a6b76b6b43a7acf06197abbe204372f75768
Sangeethasankar <sangeethasankar474@gmail.com>
2024-06-13 15:12:42 +0530

    update

---
+++ python idle/mapreducefilter.py
@@ -0,0 +1,49 @@
+# map - iterator like list & tuple 
+
+#map (fun, iterator)
+
+# def double(n):
+#     return n*2
+
+# numbers=[1,2,3,4,5]
+# result= map(double, numbers)
+# print(list(result))
+
+
+# filter method 
+
+
+# def evenNumber(n):
+#     return n%2==0
+
+# numbers=[1,2,3,4,5,6,7]
+
+# even=filter(evenNumber,numbers)
+
+# print(list(even))
+
+
+#reduce - particular func argument pass - functools - module 
+
+# import functools
+
+# numbers=[1,2,3,4,5]
+
+# product=functools.reduce (lambda x,y:x*y , numbers)
+
+# print(product)
+
+
+# import functools
+
+# def pro(x,y):
+#     return x*y
+
+# numbers=[1,2,3,4,5]
+
+# product=functools.reduce(pro,numbers)
+
+# print(product)
+
+
+
